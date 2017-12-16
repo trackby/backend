71551c2586fc5d6df64a1ba55abfedaf629dde59
sonmezonur <onursonmez6116@gmail.com>
2017-12-16 19:10:29 +0300

    Update sql files

--- sql/migrate.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE comment
-  ADD FOREIGN KEY (parent_id) REFERENCES comment(id);
-
-ALTER TABLE show_comment
-  ADD FOREIGN KEY(show_id) REFERENCES tvshow(id) ON DELETE CASCADE,
-  ADD FOREIGN KEY(comment_id) REFERENCES comment(id) ON DELETE CASCADE;

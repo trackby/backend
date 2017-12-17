bec6a34cdb2ed84e0a3f63793afa80051bc5d417
Gökcan Değirmenci <gokcan.degirmenci@ug.bilkent.edu.tr>
2017-12-18 02:44:56 +0300

    Improve user model

--- src/models/User.ts
+++ src/models/User.ts
@@ -3,24 +3,30 @@ export class User {
     private _id: number;
     private _username: string;
     private _password: string;
+    private _email: string;
+    private _age: number;
+    private _isAdmin: boolean;
 
-    constructor(id: number, username: string, password: string) {
+    constructor(username: string, password: string, email: string, age: number, isAdmin: boolean = false, id?: number) {
         this._id = id;
         this._username = username;
         this._password = password;
+        this._age = age;
+        this._email = email;
+        this._isAdmin = isAdmin;
     }
 
-    public get username(): string {
-        return this._username;
-    }
-    public set username(username: string) {
-        this._username = username;
-    }
     public get id(): number {
         return this._id;
     }
     public set id(id: number) {
         this._id = id;
+    }
+    public get username(): string {
+        return this._username;
+    }
+    public set username(username: string) {
+        this._username = username;
     }
     public get password(): string {
         return this._password;
@@ -28,4 +34,23 @@ export class User {
     public set password(password: string) {
         this._password = password;
     }
+    public get email(): string {
+        return this._email;
+    }
+    public set email(email: string) {
+        this._email = email;
+    }
+    public get age(): number {
+        return this._age;
+    }
+    public set age(age: number) {
+        this._age = age;
+    }
+    public get isAdmin(): boolean {
+        return this._isAdmin;
+    }
+    public set isAdmin(isAdmin: boolean) {
+        this._isAdmin = isAdmin;
+    }
+
 }

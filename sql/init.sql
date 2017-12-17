7cffb09292f3ed51314f78d315eccb26046dbcb7
Gökcan Değirmenci <gokcan.degirmenci@ug.bilkent.edu.tr>
2017-12-17 23:35:36 +0300

    Create users table

--- sql/init.sql
+++ sql/init.sql
@@ -1,7 +1,8 @@
-DROP TABLE IF EXISTS comment, tvshow, show_comment;
+DROP TABLE IF EXISTS comment, tvshow, show_comment, users;
+CREATE EXTENSION IF NOT EXISTS citext;
 
 /*comment table */
-CREATE TABLE comment(
+CREATE TABLE comment (
   id SERIAL,
   body   VARCHAR(255) NOT NULL,
   user_id		INT  NOT NULL,
@@ -10,7 +11,7 @@ CREATE TABLE comment(
 );
 
 /*show_comment table */
-CREATE TABLE show_comment(
+CREATE TABLE show_comment (
   id SERIAL,
   show_id	INT  NOT NULL,
   comment_id  INT  NOT NULL,
@@ -27,12 +28,24 @@ CREATE TABLE tvshow (
     PRIMARY KEY(id)
 );
 
-CREATE TABLE watch(
+CREATE TABLE watch (
   watch_id SERIAL,
   user_id INTEGER NOT NULL,
   PRIMARY KEY(watch_id)
 );
 
+CREATE TABLE users (
+  id SERIAL,
+  username VARCHAR(25) NOT NULL UNIQUE,
+  password TEXT NOT NULL,
+  email CITEXT NOT NULL UNIQUE,
+  age SMALLINT,
+  isAdmin BOOLEAN NOT NULL,
+  PRIMARY KEY(id)
+);
+
+INSERT INTO users (username, password, email, age, isAdmin) 
+VALUES ('Gokcan', 'gHgk54549232Lr3', 'gokcand@gmail.com', 22, true);
 
 /* migrations */
 ALTER TABLE comment
@@ -43,4 +56,4 @@ ALTER TABLE show_comment
   ADD FOREIGN KEY(comment_id) REFERENCES comment(id) ON DELETE CASCADE;
 
 ALTER TABLE watch
-  ADD FOREIGN KEY(user_id) REFERENCES user(id) ON DELETE CASCADE;
+  ADD FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE;

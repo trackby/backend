58ea97e956094f4d0fc70d83fc31d87d091218ce
Gökcan Değirmenci <gokcan.degirmenci@ug.bilkent.edu.tr>
2017-12-18 16:52:41 +0300

    Fix lint issues

--- src/errors/ServerError.ts
+++ src/errors/ServerError.ts
@@ -1,7 +1,7 @@
 import { Error } from './Error';
 
 export class ServerError extends Error {
-	constructor() {
-		super(500, 'Looks like we are having some trouble over here.');
-	}
+  constructor() {
+    super(500, 'Looks like we are having some trouble over here.');
+  }
 }

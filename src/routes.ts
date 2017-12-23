910e4a54e9102d066c1761e7cc02e55a73b11732
Fatih Taş <fatihtas26@gmail.com>
2017-12-23 13:20:04 +0300

    Add x-access-token to allowed headers

--- src/routes.ts
+++ src/routes.ts
@@ -28,7 +28,7 @@ export class Routes {
 
     router.use((req, res, next) => {
       res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
       next();
     });
 
